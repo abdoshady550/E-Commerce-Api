c29e964e4d96b547ca0b193bd2bafed3ba5f8b40
Abdelrahman <abdoshady550@gmail.com>
2025-08-11 19:54:44 +0300

    remove prop

--- Data/AppDbContext.cs
+++ Data/AppDbContext.cs
@@ -12,6 +12,7 @@ namespace ECommerce_Api.Data
         public DbSet<Category> Categories { get; set; }
         public DbSet<Product> Products { get; set; }
         public DbSet<CartItem> CartItems { get; set; }
+        public DbSet<Cart> Carts { get; set; }
         public DbSet<BuyingRequest> BuyingRequests { get; set; }
         public DbSet<BuyingRequestItem> BuyingRequestItems { get; set; }
 
