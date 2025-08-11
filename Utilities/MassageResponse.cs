825e40dcaf6d03bda68b8da1b83e287e8c722b5f
Abdelrahman <abdoshady550@gmail.com>
2025-08-11 03:06:22 +0300

    Use  Utilitiy static file  for maintenance and better look

---
+++ Utilities/MassageResponse.cs
@@ -0,0 +1,14 @@
+﻿namespace ECommerce_Api.Utilities
+{
+    public static class MassageResponse
+    {
+        public static string NoDataFound { get; set; } = "No Data Found";
+        public static string FailedRequest { get; set; } = "Failed Request";
+        public static string Unauthorized { get; set; } = "Invalid username or password";
+        public static string BadRequest { get; set; } = "Bad Request From The Body";
+        public static string Success { get; set; } = "Completed Successfully";
+        public static string ImageExtention { get; set; } = "only .png or .jpg Extention";
+
+
+    }
+}
